ad44f6e57a794f0a2c557a166734db80d6256d73
Hugo-Alonso <hugoalonsotoledo12@gmail.com>
2025-01-02 23:40:39 +0100

    Sign up endpoint V2

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,13 +1,18 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
+import { generateToken } from '../lib/utils.js';
 
 export const signup = async (req, res) => {
     // Collect params from the request
     const { fullName, email, password } = req.body
 
     try {
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ message: "All fields are required"});
+        }
+
         if (password.length < 6) {
-            return res.status(400).json({ message: "Password must be at least 6 characters "});
+            return res.status(400).json({ message: "Password must be at least 6 characters"});
         }
 
         const emailExists = await User.findOne({email});
@@ -26,14 +31,24 @@ export const signup = async (req, res) => {
         })
 
         if (newUser) {
-            // Generate JWT (Json Web Token)
-            
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic
+            })
+
+            res.status
         } else {
             res.status(400).json({ message: "Invalid user data" });
         }
 
     } catch (error) {
-        
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ message: "Internal Server Error"});
     }
 }
 
