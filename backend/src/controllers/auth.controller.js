9def231cdd713f9e5caeef51c22512a85111858d
Hugo-Alonso <hugoalonsotoledo12@gmail.com>
2025-01-04 22:31:35 +0100

    checkAuth endpoint done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -41,9 +41,7 @@ export const signup = async (req, res) => {
                 fullName: newUser.fullName,
                 email: newUser.email,
                 profilePic: newUser.profilePic
-            })
-
-            res.status
+            });
         } else {
             res.status(400).json({ message: "Invalid user data" });
         }
@@ -52,7 +50,7 @@ export const signup = async (req, res) => {
         console.log("Error in signup controller", error.message);
         res.status(500).json({ message: "Internal Server Error"});
     }
-}
+};
 
 export const login = async (req, res) => {
     const { email, password } = req.body
@@ -79,20 +77,20 @@ export const login = async (req, res) => {
             profilePic: user.profilePic
         })
     } catch (error) {
-        console.log("Error in login controller", error);
+        console.log("Error in login controller", error.message);
         res.status(500).json({ message: "Internal Server Error" });
     }
-}
+};
 
 export const logout = (req, res) => {
     try {
         res.cookie("jwt", "", { maxAge: 0 })
         res.status(200).json({ message: "Logged out succesfully "});
     } catch (error) {
-        console.log("Error in logout controller", error);
+        console.log("Error in logout controller", error.message);
         res.status(500).json({ message: "Internal Server Error "});
     }
-}
+};
 
 export const updateProfile = async (req, res) => {
     try {
@@ -116,3 +114,13 @@ export const updateProfile = async (req, res) => {
         res.status(500).json({ message: "Internal Server Error "});
     }
 };
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in chechAuth controller", error.message);
+        res.status(500).json({ message: "Internal Server Error "});
+    }
+
+}
