558ca6869e3087030211b4f859ef6bbcba0cc0c4
Hugo-Alonso <hugoalonsotoledo12@gmail.com>
2025-01-06 18:36:57 +0100

    getUsersForSideBar function done

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,8 +1,14 @@
+import User from "../models/user.model";
+
 export const getUsersForSideBar = async (req, res) => {
     try {
         // Your own id for later not put in at the chat side bar
         const loggedInUserId = req.user._id;
+        const filteredUsers = await User.find({_id: {$ne: loggedInUserId }}).select("-password");
+
+        res.status(200).json(filteredUsers);
     } catch (error) {
-        
+        console.log("Error in getUsersForSideBar: ", error.message);
+        res.status(500).json({ error: "Internal Server Error "}) 
     }
 }
