ba45d0708c218889e45820c96ae70e14faea3d54
Hugo-Alonso <hugoalonsotoledo12@gmail.com>
2025-01-06 19:12:25 +0100

    sendMessage V2

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -8,5 +8,5 @@ router.get("/users", protectRoute, getUsersForSideBar );
 // To be able to collect the messages between two users for the chat (History)
 router.get("/:id", protectRoute, getMessages);
 
-router.post("/send/:id", protectRoute, sendMessage)
+router.post("/send/:id", protectRoute, sendMessage);
 export default router;
