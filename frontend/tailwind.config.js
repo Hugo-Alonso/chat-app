5a32840480e3ffa76b347470ef30f07a427ee567
Hugo-Alonso <hugoalonsotoledo12@gmail.com>
2025-01-11 16:11:46 +0100

    daisyUI configuration done

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,12 @@
+import daisyui from "daisyui"
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"], // Asegúrate de incluir todas las extensiones relevantes
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    daisyui
+  ],
 };
