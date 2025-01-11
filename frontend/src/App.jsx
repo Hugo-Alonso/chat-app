ea99849a9f3436a64c8a9ba87bca4d0215100efd
Hugo-Alonso <hugoalonsotoledo12@gmail.com>
2025-01-11 16:33:32 +0100

    Routes tested and axios configuration done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,19 +4,22 @@ import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
+import HomePage from "./pages/HomePage";
+import { axiosInstance } from "./lib/axios";
 
 const App = () => {
+  
   return (
     <div>
 
       <NavBar />
 
       <Routes>
-        <Route path="/" element={ <HomePage />}/>
-        <Route path="/signup" element={ <SignUpPage />}/>
-        <Route path="/login" element={ <LoginPage />}/>
-        <Route path="/settings" element={ <SettingsPage />}/>
-        <Route path="/profile" element={ <ProfilePage />}/>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/signup" element={<SignUpPage />}/>
+        <Route path="/login" element={<LoginPage />}/>
+        <Route path="/settings" element={<SettingsPage />}/>
+        <Route path="/profile" element={<ProfilePage />}/>
 
       </Routes>
 
