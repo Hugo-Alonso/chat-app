61326af2bad9119cf3bce25132120f31670582fb
Hugo-Alonso <hugoalonsotoledo12@gmail.com>
2025-01-15 18:16:45 +0100

    navBar page User and logout icons added

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -31,7 +31,19 @@ const NavBar = () => {
             </Link>
           </div>
 
+          {authUser && (
+              <>
+                <Link to={"/profile"} className={`btn btn-sm gap-2`}>
+                  <User className="size-5" />
+                  <span className="hidden sm:inline">Profile</span>
+                </Link>
 
+                <button className="flex gap-2 items-center" onClick={logout}>
+                  <LogOut className="size-5" />
+                  <span className="hidden sm:inline">Logout</span>
+                </button>
+              </>
+            )}
         </div>
       </div>
     </header>
