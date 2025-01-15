14b0529d1bfd4f4e8bfd54ff78f0436bbbc6d671
Hugo-Alonso <hugoalonsotoledo12@gmail.com>
2025-01-15 18:12:11 +0100

    navBar page icon logo added

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,11 +1,25 @@
+import { MessageSquare } from "lucide-react";
 import { useAuthStore } from "../store/useAuthStore"
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
 
-  const { authUser } = useAuthStore;
+  const { authUser, logout } = useAuthStore;
 
   return (
-    <div>NavBar</div>
+    <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 
+    backdrop-blur-lg bg-base-100/80">
+      <div className="container mx-auto px-4 h-16">
+        <div className="flex items-center justify-between h-full">
+          <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
+                <div className="w-9 h-9 rounded-lg bg-primary/10 flex items-center justify-center">
+                  <MessageSquare className="w-5 h-5 text-primary" />
+                </div>
+                <h1 className="text-lg font-bold">Chatty</h1>
+          </Link>
+        </div>
+      </div>
+    </header>
   )
 }
 
